d71f1150965e509b4ed819799101fe2f160ee2af
Adam Goarley <agoarley@icloud.com>
2020-04-12 01:18:13 -0400

    adding site

---
+++ templates/header.php
@@ -0,0 +1,16 @@
+<header>
+    <h2>This is the Header</h2>
+    <nav class="filterNav">
+        <ul>
+            <li><a href="index.php?filter=1950">1950</a></li>
+            <li><a href="index.php?filter=1960">1960</a></li>
+            <li><a href="index.php?filter=1970">1970</a></li>
+            <li><a href="index.php?filter=1980">1980</a></li>
+            <li><a href="index.php?filter=1990">1990</a></li>
+            <li><a href="index.php?filter=All">All</a></li>
+        </ul>
+    </nav>
+
+
+    
+</header>
